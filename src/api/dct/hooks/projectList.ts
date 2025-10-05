e603e2067ae12096d839f6e40a8fbcbb1ede1a23
Edgar Adirga Firmanditya <edgar.firmanditya@tripatra.com>
2025-10-05 17:46:40 +0700

    fix fix fix

--- src/api/dct/hooks/projectList.ts
+++ src/api/dct/hooks/projectList.ts
@@ -9,13 +9,13 @@ export function useProjectQuery() {
   const { axiosGet } = useAxios()
   
   const getProjectList = () =>
-    axiosGet(`${dctHost}/api/projects`, {
+    axiosGet(`http://172.188.1.113:8080/api/projects`, {
       headers: { Authorization: `Bearer ${Cookies.get('auth')}` },
       withCredentials: false,
     })
 
   const getProjectDetail = (projectDef) =>
-    axiosGet(`${dctHost}/api/projects/${projectDef}`, {
+    axiosGet(`http://172.188.1.113:8080/api/projects/${projectDef}`, {
       headers: { Authorization: `Bearer ${Cookies.get('auth')}` },
       withCredentials: false,
     })
